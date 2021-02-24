1487531e67588abb935bd12c6b25efcfa6b1fbff
boxak <boxak@naver.com>
2021-02-25 01:56:17 +0900

    2021-02-25

---
+++ src/solutions/implementaition/Turret.java
@@ -0,0 +1,44 @@
+package solutions.implementaition;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Turret {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        int answers[] = new int[T];
+
+        for (int test = 0;test<T;test++) {
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str," ");
+
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int r1 = Integer.parseInt(st.nextToken());
+
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+
+            int square = (x2-x1)*(x2-x1)+(y2-y1)*(y2-y1);
+
+            double d = Math.sqrt(square);
+            int big = Math.max(r1,r2);
+            int small = Math.min(r1,r2);
+            if (x1==x2 && y1==y2 && r1==r2) answers[test] = -1;
+            else {
+                if (r1 + r2 > d) answers[test] = 2;
+                if (r1 + r2 == d || big==small+d) answers[test] = 1;
+                if (r1 + r2 < d || big>small+d) answers[test] = 0;
+            }
+        }
+
+        for (int answer : answers) {
+            System.out.println(answer);
+        }
+
+    }
+}
