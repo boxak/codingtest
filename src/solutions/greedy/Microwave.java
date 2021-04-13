0748fd8bb4634bfda064fdb4cfdedaea90405cf0
jwhwang <jwhwang@enliple.com>
2021-04-13 11:45:00 +0900

    2021-04-13

---
+++ src/solutions/greedy/Microwave.java
@@ -0,0 +1,12 @@
+package solutions.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Microwave {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int T = Integer.parseInt(br.readLine());
+  }
+}
