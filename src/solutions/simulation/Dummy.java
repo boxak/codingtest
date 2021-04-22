bf3340860e617056125776ea80227917a407fbe0
boxak <boxak@naver.com>
2021-04-22 23:38:29 +0900

    2021-04-22

--- src/solutions/simulation/Dummy.java
+++ src/solutions/simulation/Dummy.java
@@ -9,143 +9,140 @@ import java.util.StringTokenizer;
 // 복습이 필요한 문제!
 public class Dummy {
 
-    static int[][] map;
+    static int N,K;
     static int[] dr = {-1,0,1,0};
     static int[] dc = {0,1,0,-1};
-    static ArrayList<Pair> snake;
-    static ArrayList<Pair> snake2;
-    static int N,K,L;
-    static int[] changes;
+    static int[][] map;
     static int answer;
-    static boolean finish;
-    static boolean isEat;
+    static ArrayList<Pair> snake;
+    static int dir;
+    static int L;
+    static ArrayList<Dir> changeDir;
+
+    static class Dir {
+        int time;
+        String dir;
+        Dir(int x,String y) {
+            time = x;
+            dir = y;
+        }
+    }
 
     static class Pair {
         int r;
         int c;
-        int d;
-        Pair(int r,int c,int d) {
-            this.r = r;
-            this.c = c;
-            this.d = d;
+        Pair(int x,int y) {
+            r = x;
+            c = y;
         }
     }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         K = Integer.parseInt(br.readLine());
-        answer = 0;
-        finish = false;
 
         map = new int[N+1][N+1];
         snake = new ArrayList<>();
-        snake2 = new ArrayList<>();
-        changes = new int[10010];
+        answer = 0;
+        changeDir = new ArrayList<>();
 
         for (int i = 0;i<K;i++) {
             String str = br.readLine();
             StringTokenizer st = new StringTokenizer(str," ");
-            int apple_r = Integer.parseInt(st.nextToken());
-            int apple_c = Integer.parseInt(st.nextToken());
-            map[apple_r][apple_c] = 2;
+            int r = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            map[r][c] = 2;
         }
 
-        L = Integer.parseInt(br.readLine());
+        int L = Integer.parseInt(br.readLine());
 
         for (int i = 0;i<L;i++) {
             String str = br.readLine();
             StringTokenizer st = new StringTokenizer(str," ");
-            int time = Integer.parseInt(st.nextToken());
-            String dir = st.nextToken();
-            if ("L".equals(dir)) changes[time] = -1;
-            else changes[time] = 1;
+            int x = Integer.parseInt(st.nextToken());
+            String y = st.nextToken();
+            changeDir.add(new Dir(x,y));
         }
 
-        snake.add(new Pair(1,1,1));
+        snake.add(new Pair(1,1));
+        map[1][1] = 1;
+        dir = 1;
+        boolean eatApple = false;
+        int inx = 0;
 
         while(true) {
             answer++;
-            moveHead(answer);
-            if (finish) break;
-            moveBody();
-        }
-        System.out.println(answer);
-    }
+            eatApple = false;
+            int hr = snake.get(0).r;
+            int hc = snake.get(0).c;
 
-    static void moveHead(int time) {
-        Pair pair = snake.get(0);
-        int r = pair.r;
-        int c = pair.c;
-        int d = pair.d;
-        snake2.clear();
-        isEat = false;
+            int nhr = hr+dr[dir];
+            int nhc = hc+dc[dir];
 
-        int nr = r+dr[d];
-        int nc = c+dc[d];
-
-        if (changes[time]!=0) {
-            d = changeDir(d, changes[time]);
-        }
-        snake2.add(new Pair(nr,nc,d));
-        if (nr < 1 || nr>N || nc<1 || nc>N) {
-            finish = true;
-            return;
-        }
-        if (map[nr][nc] == 2) {
-            isEat = true;
-            map[nr][nc] = 0;
-        }
-    }
-
-    static void moveBody() {
-        for (int i = 1;i<snake.size();i++) {
-            Pair pair1 = snake.get(i-1);
-
-            snake2.add(new Pair(pair1.r,pair1.c,pair1.d));
-        }
-        if (isEat) {
-            Pair pair1 = snake.get(snake.size()-1);
-            snake2.add(new Pair(pair1.r,pair1.c,pair1.d));
-        }
-        snake.clear();
-        for (int i = 0;i<snake2.size();i++) {
-            int r = snake2.get(i).r;
-            int c = snake2.get(i).c;
-            int d = snake2.get(i).d;
-
-            if (map[r][c] == 1) {
-                finish = true;
+            if (nhr<1 || nhr>N || nhc<1 || nhc>N) {
                 break;
+            } else if (map[nhr][nhc] == 1) break;
+
+            if (map[nhr][nhc] == 2) {
+                map[nhr][nhc] = 0;
+                eatApple = true;
             }
-            map[r][c] = 1;
-            snake.add(new Pair(r,c,d));
+            ArrayList<Pair> temp = new ArrayList<>();
+            map[nhr][nhc] = 1;
+            map[hr][hc] = 0;
+            temp.add(new Pair(nhr,nhc));
+
+            if (answer == changeDir.get(inx).time) {
+                if ("L".equals(changeDir.get(inx).dir)) {
+                    dir--;
+                    if (dir == -1) dir = 3;
+                } else {
+                    dir = (dir+1)%4;
+                }
+                inx = inx==changeDir.size()-1 ? inx : inx+1;
+            }
+
+            int lastR = hr;
+            int lastC = hc;
+
+            for (int i = 1;i<snake.size();i++) {
+                int r = snake.get(i-1).r;
+                int c = snake.get(i-1).c;
+
+                int rr = snake.get(i).r;
+                int cc = snake.get(i).c;
+
+                if (i == snake.size()-1){
+                    lastR = rr;
+                    lastC = cc;
+                }
+
+                temp.add(new Pair(r,c));
+                map[rr][cc] = 0;
+                map[r][c] = 1;
+            }
+
+            if (eatApple) {
+                temp.add(new Pair(lastR,lastC));
+                map[lastR][lastC] = 1;
+            }
+
+            snake.clear();
+            for (Pair pair : temp) snake.add(new Pair(pair.r,pair.c));
+
+//            System.out.println(answer);
+//            for (int i = 1;i<=N;i++) {
+//                for (int j = 1;j<=N;j++) {
+//                    System.out.printf("%d ",map[i][j]);
+//                }
+//                System.out.println();
+//            }
+//            System.out.println();
+
         }
 
         System.out.println(answer);
-        for (int i = 1;i<=N;i++) {
-            for (int j = 1;j<=N;j++) {
-                System.out.printf("%d ",map[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println();
-
-        for (int i = 1;i<=N;i++) {
-            for (int j = 1;j<=N;j++) {
-                if (map[i][j]!=2) {
-                    map[i][j] = 0;
-                }
-            }
-        }
-    }
-
-    static int changeDir(int d, int gubun) {
-        if (gubun == -1) {
-            d = d==0 ? 3 : d-1;
-        } else {
-            d = (d+1)%4;
-        }
-        return d;
     }
 }
