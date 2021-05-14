59716a98c97d07d292a2a8f09542087b42f704bb
boxak <boxak@naver.com>
2021-05-14 20:00:41 +0900

    2021-05-14 상어 초등학교 재풀이

--- src/solutions/implementaition/SharkClass.java
+++ src/solutions/implementaition/SharkClass.java
@@ -1,142 +1,165 @@
 package solutions.implementaition;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class SharkClass {
 
+    static class Pair {
+        int student;
+        ArrayList<Integer> friends;
+        Pair(int a,int b,int c,int d,int e) {
+            student = a;
+            friends = new ArrayList<>();
+            friends.add(b);
+            friends.add(c);
+            friends.add(d);
+            friends.add(e);
+        }
+    }
+
+    static ArrayList<Pair> list;
     static int N;
-    static int[][] friends;
-    static int answer;
     static int[] dr = {-1,0,1,0};
     static int[] dc = {0,1,0,-1};
-    static int[][] map;
+    static int answer;
+    static int[][] map1;
+    static int[][] map2;
+    static int[][] map3;
+    static int maxFriends;
+    static int maxEmpty;
     static int[] scores = {0,1,10,100,1000};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         N = Integer.parseInt(br.readLine());
-        map = new int[N+1][N+1];
-        friends = new int[N*N+1][5];
+        list = new ArrayList<>();
         answer = 0;
-        for (int i = 1;i<=N*N;i++) {
+        map1 = new int[N][N];
+        map2 = new int[N][N];
+        map3 = new int[N][N];
+
+        for (int i = 0;i<N*N;i++) {
             String str = br.readLine();
             StringTokenizer st = new StringTokenizer(str," ");
-
-            for (int j = 0;j<5;j++) {
-                friends[i][j] = Integer.parseInt(st.nextToken());
-            }
+            int student = Integer.parseInt(st.nextToken());
+            int f1 = Integer.parseInt(st.nextToken());
+            int f2 = Integer.parseInt(st.nextToken());
+            int f3 = Integer.parseInt(st.nextToken());
+            int f4 = Integer.parseInt(st.nextToken());
+            list.add(new Pair(student,f1,f2,f3,f4));
         }
 
-        for (int f = 1;f<=N*N;f++) {
-            int studentNum = friends[f][0];
-            ArrayList<Integer> list = new ArrayList<>();
-            for (int j = 1;j<=4;j++) list.add(friends[f][j]);
-
-            int[][] temp = new int[N+1][N+1];
-            int maxCnt = 0;
-
-            for (int i = 1;i<=N;i++) {
-                for (int j = 1;j<=N;j++) {
-                    if (map[i][j]!=0) continue;
-                    int cnt = 0;
-                    for (int d = 0;d<4;d++) {
-                        int nr = i+dr[d];
-                        int nc = j+dc[d];
-                        if (nr<1 || nr>N || nc<1 || nc>N) continue;
-                        if (list.contains(map[nr][nc])) {
-                            cnt++;
-                        }
-                    }
-                    if (maxCnt < cnt) maxCnt = cnt;
-                    temp[i][j] = cnt;
-                }
-            }
-
-            int repeatCnt = 0;
-            int r1 = -1;
-            int c1 = -1;
-            for (int i = 1;i<=N;i++) {
-                for (int j = 1;j<=N;j++) {
-                    if (maxCnt == temp[i][j]) {
-                        repeatCnt++;
-                        r1 = i;
-                        c1 = j;
-                    }
-                }
-            }
-
-            if (repeatCnt == 1) {
-                map[r1][c1] = studentNum;
-            } else {
-                int[][] temp2 = new int[N+1][N+1];
-                int maxCnt2 = 0;
-                for (int i = 1;i<=N;i++) {
-                    for (int j = 1;j<=N;j++) {
-                        if (map[i][j]!=0) continue;
-                        if (temp[i][j] != maxCnt) continue;
-                        int cnt = 0;
-                        for (int d = 0;d<4;d++) {
-                            int nr = i+dr[d];
-                            int nc = j+dc[d];
-                            if (nr<1 || nr>N || nc<1 || nc>N) continue;
-                            if (map[nr][nc] == 0) cnt++;
-                        }
-                        if (maxCnt2<cnt) maxCnt2 = cnt;
-                        temp2[i][j] = cnt;
-                    }
-                }
-
-                for (int i = 1;i<=N;i++) {
-                    boolean flag = false;
-                    for (int j = 1;j<=N;j++) {
-                        if (map[i][j]!=0) continue;
-                        if (maxCnt2 == temp2[i][j] && temp[i][j] == maxCnt) {
-                            map[i][j] = studentNum;
-                            flag = true;
-                            break;
-                        }
-                    }
-                    if (flag) break;
-                }
-
-            }
-
-//            for (int i = 1;i<=N;i++) {
-//                for (int j = 1;j<=N;j++) {
-//                    System.out.printf("%d ",map[i][j]);
-//                }
-//                System.out.println();
-//            }
-//            System.out.println();
+        for (int i = 0;i<N*N;i++) {
+            int student = list.get(i).student;
+            ArrayList<Integer> friends = list.get(i).friends;
+            maxFriends = 0;
+            maxEmpty = 0;
+            initMap();
+            findFriends(friends);
+            findEmpty();
+            sit(student);
         }
 
-        for (int i = 1;i<=N;i++) {
-            for (int j = 1;j<=N;j++) {
-                int studentNum = map[i][j];
-                int inx = -1;
-                ArrayList<Integer> list = new ArrayList<>();
-                for (int f = 1;f<=N*N;f++) {
-                    if (friends[f][0]==studentNum) {
-                        inx = f;
-                        for (int k = 1;k<=4;k++) list.add(friends[f][k]);
+        answer = sumOfHappy();
+        System.out.println(answer);
+    }
+
+    static void initMap() {
+        for (int i = 0;i<N;i++) {
+            for (int j = 0;j<N;j++) {
+                map2[i][j] = 0;
+            }
+        }
+        for (int i = 0;i<N;i++) {
+            for (int j = 0;j<N;j++) {
+                map3[i][j] = 0;
+            }
+        }
+    }
+
+    static void findFriends(ArrayList<Integer> friends) {
+        for (int i = 0;i<N;i++) {
+            for (int j = 0;j<N;j++) {
+                if (map1[i][j]!=0) continue;
+                int cnt = 0;
+                for (int d = 0;d<4;d++) {
+                    int nr = i + dr[d];
+                    int nc = j + dc[d];
+                    if (!checkOut(nr,nc)) continue;
+                    if (friends.contains(map1[nr][nc])) {
+                        cnt++;
+                    }
+                }
+                if (cnt>maxFriends) maxFriends = cnt;
+                map2[i][j] = cnt;
+            }
+        }
+    }
+
+    static void findEmpty() {
+        for (int i = 0;i<N;i++) {
+            for (int j = 0;j<N;j++) {
+                if (map1[i][j]!=0) continue;
+                if (map2[i][j]!=maxFriends) continue;
+                int cnt = 0;
+                for (int d = 0;d<4;d++) {
+                    int nr = i + dr[d];
+                    int nc = j + dc[d];
+                    if (!checkOut(nr,nc)) continue;
+                    if (map1[nr][nc]==0) {
+                        cnt++;
+                    }
+                }
+                if (cnt > maxEmpty) maxEmpty = cnt;
+                map3[i][j] = cnt;
+            }
+        }
+    }
+
+    static void sit(int student) {
+        for (int i = 0;i<N;i++) {
+            boolean flag = false;
+            for (int j = 0;j<N;j++) {
+                if (map1[i][j]==0 && map2[i][j]==maxFriends && map3[i][j]==maxEmpty) {
+                    map1[i][j] = student;
+                    flag = true;
+                    break;
+                }
+            }
+            if (flag) break;
+        }
+    }
+
+    static boolean checkOut(int r,int c) {
+        return !(r<0 || r>=N || c<0 || c>=N);
+    }
+
+    static int sumOfHappy() {
+        int sum = 0;
+        for (int i = 0;i<N;i++) {
+            for (int j = 0;j<N;j++) {
+                int cnt = 0;
+                int student = map1[i][j];
+                ArrayList<Integer> friends = new ArrayList<>();
+                for (Pair pair : list) {
+                    if (pair.student == student) {
+                        friends = pair.friends;
                         break;
                     }
                 }
-                int cnt = 0;
+
                 for (int d = 0;d<4;d++) {
-                    int nr = i+dr[d];
-                    int nc = j+dc[d];
-                    if (nr<1 || nr>N || nc<1 || nc>N) continue;
-                    if (list.contains(map[nr][nc])) cnt++;
+                    int nr = i + dr[d];
+                    int nc = j + dc[d];
+                    if (!checkOut(nr,nc)) continue;
+                    if (friends.contains(map1[nr][nc])) {
+                        cnt++;
+                    }
                 }
-                answer+=scores[cnt];
+                sum+=scores[cnt];
             }
         }
-        System.out.println(answer);
+        return sum;
     }
 }
