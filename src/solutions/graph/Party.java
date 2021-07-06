81a8fc88e95618f46d1a5be8052b9ac4449b63ea
boxak <boxak@naver.com>
2021-07-07 01:02:49 +0900

    2021-07-07 Party 문제 통과

--- src/solutions/graph/Party.java
+++ src/solutions/graph/Party.java
@@ -24,7 +24,7 @@ public class Party {
     int N = Integer.parseInt(st.nextToken());
     int M = Integer.parseInt(st.nextToken());
     int X = Integer.parseInt(st.nextToken());
-    ArrayList<ArrayList<Pair>> vector = new ArrayList<ArrayList<>>();
+    ArrayList<ArrayList<Pair>> vector = new ArrayList<>();
 
     for (int i = 0;i<=N;i++) vector.add(new ArrayList<>());
 
@@ -39,5 +39,52 @@ public class Party {
       vector.get(a).add(new Pair(b,c));
     }
 
+    int answer = getTime(N,X,vector);
+    System.out.println(answer);
+  }
+
+  static int getTime(int N, int X, ArrayList<ArrayList<Pair>> vector) {
+    int max = Integer.MIN_VALUE;
+    for (int i = 1;i<=N;i++) {
+      int time = dijkstra(N,i,X,vector) + dijkstra(N,X,i,vector);
+      if (time > max) max = time;
+    }
+    return max;
+  }
+
+  static int dijkstra(int N,int start, int end, ArrayList<ArrayList<Pair>> vector) {
+    int[] dp = new int[N+1];
+    boolean[] check = new boolean[N+1];
+    int minDist = -1;
+
+    for (int i = 1;i<=N;i++) dp[i] = Integer.MAX_VALUE;
+
+    dp[start] = 0;
+
+    for (int i = 1;i<=N;i++) {
+      int node = -1;
+      int minValue = Integer.MAX_VALUE;
+
+      for (int j = 1;j<=N;j++) {
+        if (dp[j] < minValue && !check[j]) {
+          node = j;
+          minValue = dp[j];
+        }
+      }
+
+      if (node == end) {
+        minDist = minValue;
+        break;
+      }
+      check[node] = true;
+
+      for (int j = 0;j<vector.get(node).size();j++) {
+        int next = vector.get(node).get(j).node;
+        int time = vector.get(node).get(j).time;
+        if (dp[next] > dp[node] + time) dp[next] = dp[node] + time;
+      }
+
+    }
+    return minDist;
   }
 }
