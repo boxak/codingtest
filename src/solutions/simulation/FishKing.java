0114ef66ef2f401a382f77f5d8f4e3e2564e491e
boxak <boxak@naver.com>
2021-07-27 02:10:46 +0900

    2021-07-27 낚시왕 통과 개어려움

--- src/solutions/simulation/FishKing.java
+++ src/solutions/simulation/FishKing.java
@@ -4,18 +4,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.StringTokenizer;
 
 public class FishKing {
 	static int R,C,M;
 	static ArrayList<Shark> sharks;
+	static ArrayList<Shark> tempList;
 	static int answer;
 	static int[][] map;
+	static int[][] cmap;
 	static final int[] dr = {0,-1,1,0,0};
 	static final int[] dc = {0,0,0,1,-1};
 	static int fisher_col;
 	
-	static class Shark {
+	static class Shark implements Comparable<Shark>{
 		int r,c,s,d,z;
 		Shark(int r,int c,int s,int d,int z) {
 			this.r = r;
@@ -29,6 +32,11 @@ public class FishKing {
 		public boolean equals(Object shark) {
 			return this.r==((Shark)shark).r && this.c==((Shark)shark).c;
 		}
+		
+		@Override
+		public int compareTo(Shark shark) {
+			return shark.z-this.z;
+		}
 	}
 	
 	public static void main(String[] args) throws IOException {
@@ -41,7 +49,9 @@ public class FishKing {
 		M = Integer.parseInt(st.nextToken());
 		
 		map = new int[R][C];
+		cmap = new int[R][C];
 		sharks = new ArrayList<>();
+		tempList = new ArrayList<>();
 		
 		for (int i = 0;i<M;i++) {
 			str = br.readLine();
@@ -83,6 +93,15 @@ public class FishKing {
 	static void moveShark() {
 		int sharkCnt = sharks.size();
 		int N = 0;
+		Collections.sort(sharks);
+		tempList.clear();
+		
+		for (int i = 0;i<R;i++) {
+			for (int j = 0;j<C;j++) {
+				cmap[i][j] = 0;
+			}
+		}
+		
 		for (int i = 0;i<sharkCnt;i++) {
 			Shark shark = sharks.get(i);
 			int r = shark.r;
@@ -91,32 +110,78 @@ public class FishKing {
 			int d = shark.d;
 			int z = shark.z;
 			
-			map[r][c] = 0;
-			
 			int nr = r + s*dr[d];
 			int nc = c + s*dc[d];
 			
-			if (d==3 || d==4) {
-				if (nc<0) {
-					nc = -nc;
-					nc = nc%(2*(C-1));
-					if (nc<=C-1) {
-						d = 3;
-					} else {
+			if (nr<0) {
+				nr = -nr;
+				d = 2;
+			}
+			if (nc<0) {
+				nc = -nc;
+				d = 3;
+			}
+			// d = 2
+			if (nr>=R) {
+				int a = nr%(R-1);
+				int b = (nr-1)/(R-1);
+				if (a!=0) {
+					if (b%2==1) {
+						nr = R-1-a;
 						d = 1;
-						nc = C-nc%(C-1)-1;
+					} else {
+						nr = a;
+						d = 2;
 					}
 				} else {
-					nc = nc%(2*(C-1));
-					if (nc<C-1) {
-						d = 1;
+					if (b%2==0) {
+						nr = R-1;
+						d = 2;
 					} else {
-						d = 3;
-						nc = 2*(C-1) - nc;
+						nr = 0;
+						d = 1;
 					}
 				}
 			}
 			
+			if (nc>=C) {
+				int a = nc%(C-1);
+				int b = (nc-1)/(C-1);
+				if (a!=0) {
+					if (b%2==1) {
+						nc = C-1-a;
+						d = 4;
+					} else {
+						nc = a;
+						d = 3;
+					}
+				} else {
+					if (b%2==0) {
+						nc = C-1;
+						d = 3;
+					} else {
+						nc = 0;
+						d = 4;
+					}
+				}
+			}
+			if (cmap[nr][nc]==0) {
+				cmap[nr][nc] = z;
+				tempList.add(new Shark(nr,nc,s,d,z));
+			}
 		}
+		
+		sharks.clear();
+		
+		for (Shark shark : tempList) {
+			sharks.add(shark);
+		}
+		
+		for (int i = 0;i<R;i++) {
+			for (int j = 0;j<C;j++) {
+				map[i][j] = cmap[i][j];
+			}
+		}
+		
 	}
 }
