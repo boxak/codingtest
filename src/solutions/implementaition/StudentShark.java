6c8d060b1979010ace10c2c22ad0eb532e9ee0d8
jwhwang <jwhwang@enliple.com>
2021-07-30 08:50:47 +0900

    2021-07-30 청소년 상어 통과. 새벽이라 그런지 제정신이 아닌듯하다

--- src/solutions/implementaition/StudentShark.java
+++ src/solutions/implementaition/StudentShark.java
@@ -10,139 +10,161 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class StudentShark {
+	
+	static int answer;
+	static int[] dr = {0,-1,-1,0,1,1,1,0,-1};
+	static int[] dc = {0,0,-1,-1,-1,0,1,1,1};
+	
+	static class Fish implements Comparable<Fish>{
+		int num;
+		int d;
+		int r;
+		int c;
+		Fish(int r,int c,int num,int dir) {
+			this.r = r;
+			this.c = c;
+			this.num = num;
+			this.d = dir;
+		}
+		
+		@Override
+		public int compareTo(Fish fish) {
+			return this.num-fish.num;
+		}
+		
+		@Override
+		public boolean equals(Object fish) {
+			return this.num==((Fish)fish).num;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		Fish[][] map = new Fish[4][4];
+		ArrayList<Fish> fishes = new ArrayList<>();
+		answer = Integer.MIN_VALUE;
+		
+		for (int i = 0;i<4;i++) {
+			String str  = br.readLine();
+			StringTokenizer st = new StringTokenizer(str," ");
+			for (int j = 0;j<4;j++) {
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				map[i][j] = new Fish(i,j,a,b);
+				fishes.add(new Fish(i,j,a,b));
+			}
+		}
 
-  static class Fish implements Comparable<Fish>{
-    int inx;
-    int r;
-    int c;
-    int dir;
-
-    Fish(){}
-
-    Fish(int x,int y,int z,int w) {
-      inx = x;
-      r = y;
-      c = z;
-      dir = w;
-    }
-
-    public int compareTo(Fish fish) {
-      return this.inx - fish.inx;
-    }
-
-    public boolean equals(Object fish) {
-      return ((Fish)fish).inx == this.inx;
-    }
-  }
-
-  static int[] dr = {0,-1,-1,0,1,1,1,0,-1};
-  static int[] dc = {0,0,-1,-1,-1,0,1,1,1};
-  static int answer = 0;
-
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    Fish[][] map = new Fish[4][4];
-    ArrayList<Fish> fishs = new ArrayList<>();
-    for (int i = 0;i<4;i++) {
-      String str = br.readLine();
-      StringTokenizer st = new StringTokenizer(str," ");
-      for (int j = 0;j<4;j++) {
-        int inx = Integer.parseInt(st.nextToken());
-        int dir = Integer.parseInt(st.nextToken());
-        map[i][j] = new Fish(inx,i,j,dir);
-        fishs.add(new Fish(inx,i,j,dir));
-      }
-    }
-
-    Collections.sort(fishs);
-
-    int sharkR = 0;
-    int sharkC = 0;
-    int sharkD = 0;
-
-    dfs(map,fishs,sharkR,sharkC,sharkD,0);
-
-    System.out.println(answer);
-  }
-
-  static void dfs(Fish[][] map, ArrayList<Fish> list,int sharkR,int sharkC,int sharkD,int eatSum) {
-    sharkD = map[sharkR][sharkC].dir;
-    int inx = map[sharkR][sharkC].inx;
-    list.remove(map[sharkR][sharkC]);
-    map[sharkR][sharkC] = new Fish();
-    if (eatSum+inx>answer) answer = eatSum+inx;
-
-    Map<String,Object> objMap = fishMove(map,list,sharkR,sharkC);
-
-    map = (Fish[][]) objMap.get("map");
-    list = (ArrayList<Fish>) objMap.get("list");
-
-    for (int i = 1;i<4;i++) {
-      int nr = sharkR+dr[sharkD]*i;
-      int nc = sharkC+dc[sharkD]*i;
-      if (nr<0 || nr>=4 || nc<0 || nc>=4) break;
-      if (map[nr][nc].inx!=0) {
-        Fish[][] tempMap = new Fish[4][4];
-        ArrayList<Fish> tempList = new ArrayList<>();
-        for (int j = 0;j<4;j++) {
-          for (int k = 0;k<4;k++) {
-            Fish tempFish = new Fish(map[j][k].inx,map[j][k].r,map[j][k].c,map[j][k].dir);
-            tempMap[j][k] = tempFish;
-          }
-        }
-        for (Fish fish : list) {
-          tempList.add(new Fish(fish.inx,fish.r,fish.c,fish.dir));
-        }
-        dfs(map,list,nr,nc,sharkD,eatSum+inx);
-        for (int j = 0;j<4;j++) {
-          for (int k = 0;k<4;k++) {
-            map[j][k] = new Fish(tempMap[j][k].inx,tempMap[j][k].r,tempMap[j][k].c,tempMap[j][k].dir);
-          }
-        }
-        list.clear();
-        for (Fish fish : tempList) {
-          list.add(new Fish(fish.inx,fish.r,fish.c,fish.dir));
-        }
-      }
-    }
-  }
-
-  static Map<String,Object> fishMove(Fish[][] map, ArrayList<Fish> list,int sharkR,int sharkC) {
-    Map<String,Object> objMap = new HashMap<>();
-    for (int i = 0;i<list.size();i++) {
-      Fish fish = list.get(i);
-      int r = fish.r;
-      int c = fish.c;
-      int d = fish.dir;
-      int inx = fish.inx;
-
-      int nr = r+dr[d];
-      int nc = c+dc[d];
-      while((nr<0 || nr>=4 || nc<0 || nc>=4) || (nr==sharkR && nc==sharkC)) {
-        d = d==8 ? 1 : d+1;
-        nr = r+dr[d];
-        nc = c+dc[d];
-      }
-
-      Fish fish2 = map[nr][nc];
-
-      if (fish2.inx!=0) {
-        int inx2 = fish2.inx;
-        int pos2 = list.indexOf(fish2);
-        fish2.r = r;
-        fish2.c = c;
-        list.set(pos2, fish2);
-        list.set(i, new Fish(inx, nr, nc, d));
-        map[nr][nc] = new Fish(inx, nr, nc, d);
-        map[r][c] = fish2;
-      } else {
-        list.set(i, new Fish(inx, nr, nc, d));
-        map[nr][nc] = new Fish(inx, nr, nc, d);
-        map[r][c] = new Fish();
-      }
-    }
-    objMap.put("map",map);
-    objMap.put("list",list);
-    return objMap;
-  }
+		dfs(map,fishes,0,0,0);
+		
+		System.out.println(answer);
+		
+	}
+	
+	static void dfs(Fish[][] map,ArrayList<Fish> fishes,int r,int c,int sum) {
+		
+		Fish[][] cmap = copyMap(map);
+		ArrayList<Fish> clist = copyList(fishes);
+		
+		int inx = cmap[r][c].num;
+		int dir = cmap[r][c].d;
+		if (sum+inx>answer) answer = sum+inx;
+		
+		cmap[r][c] = new Fish(r,c,0,0);
+		clist.remove(new Fish(0,0,inx,0));
+		
+		moveFish(cmap,clist,r,c);
+		
+		for (int go = 1;go<=3;go++) {
+			int nr = r+go*dr[dir];
+			int nc = c+go*dc[dir];
+			if (isOut(nr,nc)) break;
+			if (cmap[nr][nc].num!=0) {
+				dfs(cmap,clist,nr,nc,sum+inx);
+			}
+		}
+		
+		
+	}
+	
+	static void moveFish(Fish[][] map,ArrayList<Fish> fishes,int sharkR,int sharkC) {
+		Collections.sort(fishes);
+		int len = fishes.size();
+		for (int i = 0;i<len;i++) {
+			Fish fish = fishes.get(i);
+			
+			int r = fish.r;
+			int c = fish.c;
+			int d = fish.d;
+			int num = fish.num;
+			
+			d = changeDir(map,sharkR,sharkC,r,c,d);
+			if (d==-1) continue;
+			
+			int nr = r+dr[d];
+			int nc = c+dc[d];
+			
+			if (map[nr][nc].num==0) {
+				map[nr][nc] = new Fish(nr,nc,num,d);
+				fishes.set(i, new Fish(nr,nc,num,d));
+				map[r][c] = new Fish(r,c,0,0);
+			} else {
+				int index = fishes.indexOf(new Fish(0,0,map[nr][nc].num,0));
+				
+				int num2 = map[nr][nc].num;
+				int dir2 = map[nr][nc].d;
+				map[nr][nc] = new Fish(nr,nc,num,d);
+				fishes.set(i, new Fish(nr,nc,num,d));
+				
+				map[r][c] = new Fish(r,c,num2,dir2);
+				fishes.set(index, new Fish(r,c,num2,dir2));
+			}
+			
+		}
+	}
+	
+	static Fish[][] copyMap(Fish[][] map) {
+		Fish[][] cmap = new Fish[4][4];
+		for (int i = 0;i<4;i++) {
+			for (int j = 0;j<4;j++) {
+				cmap[i][j] = new Fish(i,j,map[i][j].num,map[i][j].d);
+			}
+		}
+		return cmap;
+	}
+	
+	static ArrayList<Fish> copyList(ArrayList<Fish> list) {
+		ArrayList<Fish> clist = new ArrayList<>();
+		for (Fish fish : list) {
+			clist.add(new Fish(fish.r,fish.c,fish.num,fish.d));
+		}
+		return clist;
+	}
+	
+	static int changeDir(Fish[][] map,int sharkR,int sharkC,int r,int c,int d) {
+		int tempD = d;
+		boolean find = false;
+		for (int i = 0;i<=8;i++) {
+			int nr = r+dr[tempD];
+			int nc = c+dc[tempD];
+			if (isOut(nr,nc) || (nr==sharkR && nc==sharkC)) {
+				tempD++;
+				if (tempD==9) tempD = 1;
+				continue;
+			} else {
+				find = true;
+				break;
+			}
+		}
+		
+		if (!find) tempD = -1;
+		
+		return tempD;
+	}
+	
+	static boolean isOut(int r,int c) {
+		return r<0 || r>=4 || c<0 || c>=4;
+	}
+  
 }
