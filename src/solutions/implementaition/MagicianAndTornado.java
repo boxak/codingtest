4a84c26b3e4d17fe7c313278250a2ea43571b59d
jwhwang <jwhwang@enliple.com>
2021-08-04 14:34:59 +0900

    2021-08-04 마법사 상어와 토네이도 통과.

--- src/solutions/implementaition/MagicianAndTornado.java
+++ src/solutions/implementaition/MagicianAndTornado.java
@@ -6,96 +6,118 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class MagicianAndTornado {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    int N = Integer.parseInt(br.readLine());
-    int[][] Ar = new int[N][N];
-    int[] Dr = new int[N*N];
-    int answer = 0;
+	static int N;
+	static int[][] map;
+	static int[] dr = {0,1,0,-1};
+	static int[] dc = {-1,0,1,0};
+	
+	static int[] dr2 = {0,0,1,1,1,2,-1,-1,-1,-2};
+	static int[] dc2 = {-1,-2,-1,0,1,0,1,0,-1,0};
+	static double[] rates = {0,0.05,0.1,0.07,0.01,0.02,0.01,0.07,0.1,0.02};
+	static int[][] tempMap = new int[6][6];
+	static int[][] tempMap2 = new int[6][6];
+	
+	static int answer;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		map = new int[N+1][N+1];
+		
+		for (int i = 1;i<=N;i++) {
+			String str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str," ");
+			for (int j = 1;j<=N;j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		simulation();
+		
+		System.out.println(answer);
+	}
 
-    int[] dr = {-1,0,1,0};
-    int[] dc = {0,1,0,-1};
+	static void simulation() {
+		int r = N/2+1;
+		int c = N/2+1;
+		int d = 0;
+		int goCount = 0;
+		int goCountLimit = 1;
+		int changeDir = 0;
+		while(r!=1 || c!=1) {
+			r = r + dr[d];
+			c = c + dc[d];
+			moveSand(r,c,d);
+			goCount++;
+			if (goCountLimit==goCount) {
+				goCount = 0;
+				d = (d+1)%4;
+				if (changeDir==0) {
+					changeDir++;
+				} else if (changeDir==1){
+					changeDir = 0;
+					goCountLimit++;
+				}
+			}
+		}
+	}
+	
+	static void moveSand(int r,int c,int d) {
+		int initSand = map[r][c];
+		int sandSum = 0;
+		
+		for (int i=0;i<=5;i++) {
+			for (int j = 0;j<=5;j++) {
+				tempMap[i][j] = 0;
+				tempMap2[i][j] = 0;
+			}
+		}
+		
+		int tempR = r - (r-3);
+		int tempC = c - (c-3);
+		
+		for (int i = 1;i<=9;i++) {
+			double rate = rates[i];
+			int nr = tempR + dr2[i];
+			int nc = tempC + dc2[i];
+			int movesands = (int)(rate*initSand);
 
+			tempMap[nr][nc]=movesands;
+			sandSum+=movesands;
+		}
+		int nr = tempR+dr2[0];
+		int nc = tempC+dc2[0];
+		
+		int movesands = initSand - sandSum;
 
-    double[] rate = {0.01,0.07,0.1,0.02,0.05,0.1,0.07,0.01,0.02};
-    int[] ndr = {1,0,-1,0,-2,-1,0,1,0};
-    int[] ndc = {1,1,1,2,0,-1,-1,-1,-2};
-
-    int[] edr = {-1,-1,-1,-2,0,1,1,1,2};
-    int[] edc = {-1,0,1,0,2,1,0,-1,0};
-
-    int[] sdr = {-1,0,1,0,2,1,0,-1,0};
-    int[] sdc = {-1,-1,-1,-2,0,1,1,1,2};
-
-    int[] wdr = {-1,-1,-1,-2,0,1,1,1,2};
-    int[] wdc = {1,0,-1,0,-2,-1,0,1,0};
-
-    for (int i = 0;i<N;i++) {
-      String str = br.readLine();
-      StringTokenizer st = new StringTokenizer(str," ");
-      for (int j = 0;j<N;j++) {
-        Ar[i][j] = Integer.parseInt(st.nextToken());
-      }
-    }
-
-    int inx = 0;
-    for (int i = 0;i<N/2;i++) {
-      int cnt1 = 2*i+1;
-      int cnt2 = 2*i+1;
-      int cnt3 = 2*i+2;
-      int cnt4 = 2*i+2;
-
-      for (int j = 0;j<cnt1;j++) Dr[inx++] = 3;
-      for (int j = 0;j<cnt2;j++) Dr[inx++] = 2;
-      for (int j = 0;j<cnt3;j++) Dr[inx++] = 1;
-      for (int j = 0;j<cnt4;j++) Dr[inx++] = 0;
-    }
-
-    for (int i = 0;i<N;i++) {
-      Dr[N*N-i-1] = 3;
-    }
-
-    int r = N/2;
-    int c = N/2;
-    for (int i = 0;i<N*N;i++) {
-      int d = Dr[i];
-      int nr = r+dr[d];
-      int nc = c+dc[d];
-      if (nr<0 || nr>=N || nc<0 || nc>=N) break;
-      r = nr;
-      c = nc;
-      int y = Ar[r][c];
-      for (int j = 0;j<9;j++) {
-        int r2 = 0;
-        int c2 = 0;
-        if (d == 0) {
-          r2 = r+ndr[j];
-          c2 = c+ndc[j];
-        }
-        if (d == 1) {
-          r2 = r + edr[j];
-          c2 = c + edc[j];
-        }
-        if (d == 2) {
-          r2 = r+sdr[j];
-          c2 = c+sdc[j];
-        }
-        if (d==3) {
-          r2 = r+wdr[j];
-          c2 = c+wdc[j];
-        }
-        int mass = (int)(y*rate[j]);
-        if (r2<0 || r2>=N || c2<0 || c2>=N) answer+=mass;
-        else Ar[r2][c2]+=mass;
-        Ar[r][c]-=mass;
-      }
-      int nr2 = r+dr[d];
-      int nc2 = c+dc[d];
-      if (nr2<0 || nr2>=N || nc2<0 || nc2>=N) answer+=Ar[r][c];
-      else Ar[nr2][nc2]+=Ar[r][c];
-      Ar[r][c] = 0;
-
-    }
-    System.out.println(answer);
-  }
+		tempMap[nr][nc] = movesands;
+		
+		for (int dd=0;dd<d;dd++) {
+			for (int i = 1;i<=5;i++) {
+				for (int j = 1;j<=5;j++) {
+					tempMap2[5+1-j][i] = tempMap[i][j];
+				}
+			}
+			
+			for (int i = 1;i<=5;i++) {
+				for (int j = 1;j<=5;j++) {
+					tempMap[i][j] = tempMap2[i][j];
+				}
+			}
+		}
+		
+		for (int i = 1;i<=5;i++) {
+			for (int j = 1;j<=5;j++) {
+				if(i+r-3<1 || i+r-3>N || j+c-3<1 || j+c-3>N) {
+					answer+=tempMap[i][j];
+				} else {
+					map[i+r-3][j+c-3]+=tempMap[i][j];
+				}
+			}
+		}
+		
+		map[r][c] = 0;
+		
+	}
 }
