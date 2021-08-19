6422f4bcdc1688ed61bf4d3783d787335b126618
jwhwang <jwhwang@enliple.com>
2021-08-19 17:03:04 +0900

    2018-08-19 홈방범 서비스 통과

--- src/solutions/bruteforce/HomeSecurityService.java
+++ src/solutions/bruteforce/HomeSecurityService.java
@@ -7,67 +7,68 @@ import java.util.StringTokenizer;
 
 public class HomeSecurityService {
 
-  static int N,M;
-  static int[][] map;
-
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    //BufferedReader br = new BufferedReader(new FileReader(new File("C:\\Users\\enliple\\Downloads\\sample_input (2).txt")));
-
-    int TestCnt = Integer.parseInt(br.readLine());
-
-    for (int test = 1;test<=TestCnt;test++) {
-      String str = br.readLine();
-      StringTokenizer st = new StringTokenizer(str," ");
-
-      N = Integer.parseInt(st.nextToken());
-      M = Integer.parseInt(st.nextToken());
-
-      map = new int[N+1][N+1];
-
-      for (int i = 1;i<=N;i++) {
-        str = br.readLine();
-        st = new StringTokenizer(str," ");
-        for (int j = 1;j<=N;j++) {
-          map[i][j] = Integer.parseInt(st.nextToken());
-        }
-      }
-
-      int answer = getMaxHouseCnt();
-      System.out.println("#"+test+" "+answer);
-    }
-
-  }
-
-  static int getMaxHouseCnt() {
-    int maxCnt = Integer.MIN_VALUE;
-
-    for (int r = 1;r<=N;r++) {
-      for (int c = 1;c<=N;c++) {
-        for (int dist = 0;dist<=2*N-1;dist++) {
-          int cnt = getHouseCnt(r,c,dist);
-          int cost = (dist+1)*(dist+1) + dist*dist;
-          int revenue = cnt*M - cost;
-          if (revenue>=0 && cnt>maxCnt) maxCnt = cnt;
-        }
-      }
-    }
-
-    return maxCnt;
-  }
-
-  static int getHouseCnt(int r,int c,int dist) {
-    int houseCnt = 0;
-    for (int i = 1;i<=N;i++) {
-      for (int j = 1;j<=N;j++) {
-        if (map[i][j]==1 && getDist(r,c,i,j)<=dist) houseCnt++;
-      }
-    }
-
-    return houseCnt;
-  }
-
-  static int getDist(int r1,int c1,int r2,int c2) {
-    return Math.abs(r2-r1) + Math.abs(c2-c1);
-  }
+	static int N,M;
+	static int[][] map;
+	static int answer;
+	static int houseCnt;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int TestCnt = Integer.parseInt(br.readLine());
+		int[] answers = new int[TestCnt+1];
+		
+		for (int test = 1;test<=TestCnt;test++) {
+			String str = br.readLine();
+			StringTokenizer st = new StringTokenizer(str," ");
+			
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			
+			map = new int[N+1][N+1];
+			houseCnt = 0;
+			answer = 0;
+			
+			for (int i = 1;i<=N;i++) {
+				str = br.readLine();
+				st = new StringTokenizer(str," ");
+				for (int j = 1;j<=N;j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+					if (map[i][j]==1) houseCnt++;
+				}
+			}
+			
+			for (int i = 1;i<=N;i++) {
+				for (int j = 1;j<=N;j++) {
+					for (int k = 1;;k++) {
+						int dist = k-1;
+						int tempCnt = 0;
+						
+						for (int r = 1;r<=N;r++) {
+							for (int c = 1;c<=N;c++) {
+								if (Math.abs(r-i)+Math.abs(c-j)<=dist && map[r][c]==1) {
+									tempCnt++;
+								}
+							}
+						}
+						
+						int revenue = M*tempCnt - k*k-(k-1)*(k-1);
+						if (revenue>=0) {
+							if (tempCnt>answer) answer = tempCnt;
+						}
+						
+						
+						if (tempCnt==houseCnt) break;
+					}
+				}
+			}
+			
+			answers[test] = answer;
+			
+		}
+		
+		for (int i = 1;i<=TestCnt;i++) System.out.println("#"+i+" "+answers[i]);
+		
+	}
+	
 }
