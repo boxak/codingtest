87bc275b5f17b372b310c0653a66a427d1000102
jwhwang <jwhwang@enliple.com>
2021-08-23 18:01:22 +0900

    2021-08-23 활주로 건설 통과

--- src/solutions/implementaition/BuildAirstrip.java
+++ src/solutions/implementaition/BuildAirstrip.java
@@ -9,7 +9,7 @@ public class BuildAirstrip {
 
 	static int N,X;
 	static int[][] map;
-	static int[][] check;
+	static int[] arr;
 	static int answer;
 	
 	public static void main(String[] args) throws IOException {
@@ -27,7 +27,7 @@ public class BuildAirstrip {
 			answer = 0;
 			
 			map = new int[N+1][N+1];
-			check = new int[N+1][N+1];
+			arr = new int[N+1];
 			
 			for (int i = 1;i<=N;i++) {
 				str = br.readLine();
@@ -37,11 +37,8 @@ public class BuildAirstrip {
 				}
 			}
 			
-			makeAirstrip(0);
-			checkPossible(0);
-			makeAirstrip(1);
-			checkPossible(1);
-			
+			makeAirstripRow();
+			makeAirstripCol();		
 			answers[test] = answer;
 			
 		}
@@ -50,113 +47,68 @@ public class BuildAirstrip {
 		
 	}
 	
-	static void makeAirstrip(int gubun) {
+	static void makeAirstripRow() {
 		for (int i = 1;i<=N;i++) {
-			for (int j = 1;j<=N;j++) {
-				check[i][j] = 0;
-			}
-		}
-		
-		int row1 = 0;
-		int col1 = 0;
-		int row2 = 0;
-		int col2 = 0;
-		int row3 = 0;
-		int col3 = 0;
-		
-		for (int i = 1;i<=N;i++) {
-			for (int j = 2;j<=N-1;j++) {
-				for (int k = -1;k<=1;k+=2) {
-					
-					if (gubun==0) {
-						row1 = i;
-						col1 = j;
-						
-						row2 = i;
-						col2 = j+k;
-					} else {
-						row1 = j;
-						col1 = i;
-						
-						row2 = j+k;
-						col2 = i;
-					}
-					
-					if (map[row1][col1]-map[row2][col2]==1) {
-						for (int f = 1;f<=X;f++) {
-							
-							if (gubun==0) {
-								row3 = i;
-								col3 = j+f*k;
-							} else {
-								row3 = j+f*k;
-								col3 = i;
-							}
-							if (row3<1 || row3>N || col3<1 || col3>N) break;
-							
-							check[row3][col3]++;
-						}
-						
-					}
-				}
-			}
-		}
-		
-	}
-	
-	static void checkPossible(int gubun) {
-		
-		int row1 = 0;
-		int col1 = 0;
-		int row2 = 0;
-		int col2 = 0;
-		
-		for (int i = 1;i<=N;i++) {
+			for (int j = 1;j<=N;j++) arr[j] = 0;
 			boolean flag = true;
-			Roop1 : for (int j = 2;j<=N-1;j++) {
+			Roop1 : for (int j = 1;j<=N;j++) {
 				for (int k = -1;k<=1;k+=2) {
-					
-					if (gubun==0) {
-						row1 = i;
-						col1 = j;
-						
-						row2 = i;
-						col2 = j+k;
-					} else {
-						row1 = j;
-						col1 = i;
-						
-						row2 = j+k;
-						col2 = i;
-					}
-					
-					if (map[row1][col1]-map[row2][col2]>1) {
+					if ((j==1 && k==-1) || (j==N && k==1)) continue;
+					if (map[i][j]-map[i][j+k]>=2) {
 						flag = false;
 						break Roop1;
-					}
-					if (map[row1][col1]-map[row2][col2]==1 && check[row2][col2]==0) {
-						flag = false;
-						break Roop1;
-					}
-					if (map[row1][col1]-map[row2][col2]==1 && (j+X*k<1 || j+X*k>N)) {
-						flag = false;
-						break Roop1;
+					} else if (map[i][j]-map[i][j+k]==1) {
+						for (int h=1;h<=X;h++) {
+							if (j+h*k<1 || j+h*k>N) {
+								flag = false;
+								break Roop1;
+							}
+							arr[j+h*k]++;
+						}
 					}
 				}
 			}
-			Roop2 : for (int j = 1;j<=N;j++) {
-				
-				if (gubun==0) {
-					row1 = i;
-					col1 = j;
-				} else {
-					row1 = j;
-					col1 = i;
+			
+			if (flag) {
+				for (int j = 1;j<=N;j++) {
+					if (arr[j]>=2) {
+						flag = false;
+						break;
+					}
 				}
-				
-				if (check[row1][col1]>1) {
-					flag = false;
-					break Roop2;
+			}
+			if (flag) answer++;
+		}
+	} 
+	
+	static void makeAirstripCol() {
+		for (int j = 1;j<=N;j++) {
+			for (int i = 1;i<=N;i++) arr[i] = 0;
+			boolean flag = true;
+			Roop1 : for (int i = 1;i<=N;i++) {
+				for (int k = -1;k<=1;k+=2) {
+					if ((i==1 && k==-1) || (i==N && k==1)) continue;
+					if (map[i][j]-map[i+k][j]>=2) {
+						flag = false;
+						break Roop1;
+					} else if(map[i][j]-map[i+k][j]==1) {
+						for (int h=1;h<=X;h++) {
+							if (i+h*k<1 || i+h*k>N) {
+								flag = false;
+								break Roop1;
+							}
+							arr[i+h*k]++;
+						}
+					
+					}
+				}
+			}
+			if (flag) {
+				for (int i = 1;i<=N;i++) {
+					if (arr[i]>=2) {
+						flag = false;
+						break;
+					}
 				}
 			}
 			if (flag) answer++;
