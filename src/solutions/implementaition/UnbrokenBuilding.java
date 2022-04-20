2c580711e70d889479bd9cd786a3c5ff250e4c22
boxak <boxak@naver.com>
2022-04-20 21:29:12 +0900

    2022-04-20

--- src/solutions/implementaition/UnbrokenBuilding.java
+++ src/solutions/implementaition/UnbrokenBuilding.java
@@ -3,6 +3,7 @@ package solutions.implementaition;
 public class UnbrokenBuilding {
     public int solution(int[][] board, int[][] skill) {
         int answer = 0;
+
         return answer;
     }
 }
