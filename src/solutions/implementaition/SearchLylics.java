8c0871f43a05fc3f66875ea33ac7667b0c001c6d
boxak <boxak@naver.com>
2022-08-11 23:21:45 +0900

    2022-08-11 가사 검색 푸는 중

---
+++ src/solutions/implementaition/SearchLylics.java
@@ -0,0 +1,13 @@
+package solutions.implementaition;
+
+public class SearchLylics {
+    public static int[] solution(String[] words, String[] queries) {
+        int[] answer = {};
+
+
+
+        return answer;
+    }
+
+
+}
