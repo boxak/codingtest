c5a7af75c6d4a0c9aed1333867671cce7a7656b4
boxak <boxak@naver.com>
2022-08-12 17:53:07 +0900

    2022-08-12 가사 검색 푸는 중

--- src/solutions/implementaition/SearchLylics.java
+++ src/solutions/implementaition/SearchLylics.java
@@ -1,5 +1,8 @@
 package solutions.implementaition;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class SearchLylics {
     public static int[] solution(String[] words, String[] queries) {
         int[] answer = {};
@@ -9,5 +12,31 @@ public class SearchLylics {
         return answer;
     }
 
+    public static void main(String[] args) {
+        String s1 = "fro??";
+        String s2 = "frod?";
 
+        ArrayList<String> list = new ArrayList<>();
+        list.add(s1);
+        list.add(s2);
+
+        System.out.println("starts : " + s2.startsWith(""));
+
+        Collections.sort(list);
+
+        System.out.println(list);
+
+        for(String str : list) {
+            System.out.println(str);
+        }
+
+//        String[] words = {"frodo", "front", "frost", "frozen", "frame", "kakao"};
+//        String[] queries = {"fro??", "????o", "fr???", "fro???", "pro?"};
+//
+//        int[] results = solution(words, queries);
+//
+//        for(int num : results) {
+//            System.out.printf("%d ",num);
+//        }
+    }
 }
