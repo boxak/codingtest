4aea56435a5ffd1e8014f5ab493e71f3832d30bc
boxak <boxak@naver.com>
2022-08-16 23:03:23 +0900

    2022-08-16 가사 검색 푸는 중

--- src/solutions/implementaition/SearchLylics.java
+++ src/solutions/implementaition/SearchLylics.java
@@ -2,10 +2,60 @@ package solutions.implementaition;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SearchLylics {
+
+    static class Node {
+        Map<Character, Node> childNode = new HashMap<>();
+        boolean isEnd;
+        int cnt;
+    }
+
+    static class Trie {
+        Node root = new Node();
+
+        void insert(String s) {
+
+            Node node = this.root;
+
+            for (char c : s.toCharArray()) {
+                if (node.childNode.containsKey(c)) {
+                    node = node.childNode.get(c);
+                } else {
+                    node.childNode.put(c, new Node());
+                    node.cnt++;
+                    node = node.childNode.get(c);
+                }
+            }
+
+            node.isEnd = true;
+        }
+
+        int search(String s) {
+            Node node = this.root;
+
+            for (char c : s.toCharArray()) {
+                node = node.childNode.getOrDefault(c, null);
+                if (node == null) {
+                    return node.cnt;
+                }
+            }
+
+            return 0;
+        }
+
+    }
+
     public static int[] solution(String[] words, String[] queries) {
         int[] answer = {};
+
+        Trie[] tries = new Trie[10001];
+
+        for (int i = 0;i< tries.length;i++) {
+            tries[i] = new Trie();
+        }
 
 
 
