e9e87a0b795387df9a3ea47956b11b5f2864e8b9
boxak <boxak@naver.com>
2022-09-17 21:39:07 +0900

    2022-09-17

--- src/solutions/implementaition/TheBestSet.java
+++ src/solutions/implementaition/TheBestSet.java
@@ -1,51 +1,29 @@
 package solutions.implementaition;
 
-public class TheBestSet {
+import java.util.Arrays;
 
-    static long answer = Long.MIN_VALUE;
-    static int arr[];
-    static int answerList[];
+public class TheBestSet {
 
     static public int[] solution(int n, int s) {
         int[] answer = {};
-        arr = new int[n];
-        answerList = new int[n];
 
-        for (int i = s;i>=1;i--) {
-            visit(n, s, i, 0, 1);
-        }
+        int num = s / n;
+
+        if (num == 0) return new int[]{-1};
 
         answer = new int[n];
 
-        for (int i = 0;i<n;i++) {
-            answer[i] = answerList[i];
-        }
+        double exact = ((double)s) / n;
+
+        if (exact - num >= 0.5)
+
+        Arrays.sort(answer);
 
         return answer;
     }
 
-    static void visit(int n, int s, int cur, int x, long product) {
-        if (n-1 == x) {
-            arr[x] = cur;
-            if (answer < product * cur) {
-                answer = product * cur;
-                for (int i = 0; i < n; i++) {
-                    answerList[i] = arr[i];
-                }
-            }
-            return;
-        }
-
-        arr[x] = cur;
-        int nextS = s - cur;
-        for (int i = nextS;i>=1;i--) {
-            visit(n, nextS, i, x+1, product * cur);
-        }
-
-    }
-
     public static void main(String[] args) {
-        int[] arr = solution(2, 100_000_000);
+        int[] arr = solution(100, 100_000_000);
 
         System.out.println("answer");
         for (int i = 0;i<arr.length;i++) {
